099e0b453fea4842ce52f62cc57160e7ac7fa563
João Victor <jvna07@gmail.com>
2025-02-24 18:00:56 -0300

    Ajuste de classe Contador

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -16,7 +16,7 @@ public class Contador {
 			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 
 		}
-		
+		terminal.close();
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
